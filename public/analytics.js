60f8e8702344282895c931a04a4d15ada08391b0
Jayly <65847850+JaylyDev@users.noreply.github.com>
2026-04-13 21:07:10 +0100

    Squashed commit of the following:
    commit 877a13c899caa611ee11cbd2b31060bec1f025e0
    Author: Jayly <65847850+JaylyDev@users.noreply.github.com>
    Date:   Thu Mar 12 08:28:37 2026 +0000
    
        Update analytics.js
    
    commit a19da65075927dbd948134cfa54cfe4a1da677ef
    Author: Jayly <65847850+JaylyDev@users.noreply.github.com>
    Date:   Thu Mar 12 07:51:39 2026 +0000
    
        Add 1600p
    
    commit 1b4394215f8c0397743f43ce0239da9288fa20c1
    Author: Jayly <65847850+JaylyDev@users.noreply.github.com>
    Date:   Tue Mar 10 23:38:35 2026 +0000
    
        this should work

---
+++ public/analytics.js
@@ -0,0 +1,25 @@
+const GA_MEASUREMENT_ID = "G-Q3X0X9VRB2";
+const script = document.createElement("script");
+script.src = "https://www.googletagmanager.com/gtag/js?id=" + GA_MEASUREMENT_ID;
+script.async = true;
+document.head.appendChild(script);
+
+window.dataLayer = window.dataLayer || [];
+function gtag() {
+  dataLayer.push(arguments);
+}
+gtag("js", new Date());
+
+const screenResolution = window.screen.width + "x" + window.screen.height;
+const blockedResolutions = new Set([
+  "1280x1200",
+  "800x600",
+  "1600x1600",
+  "3840x2160",
+  "0x0",
+  "1024x1024",
+]);
+const trafficType = !blockedResolutions.has(screenResolution) ? "internal" : "external";
+gtag("config", GA_MEASUREMENT_ID, {
+  traffic_type: trafficType,
+});
